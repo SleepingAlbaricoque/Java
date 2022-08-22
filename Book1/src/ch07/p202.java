090414d24427eca57131b7b6a9e6f3c90da2dbd8
SleepingAlbaricoque <tnqls0421@naver.com>
2022-08-22 11:02:33 +0900

    2022.08.22 내용업데이트

--- Book1/src/ch07/p202.java
+++ Book1/src/ch07/p202.java
@@ -1,5 +1,17 @@
 package ch07;
 
+/* 날짜 : 2022.08.22
+ * 이름 : 조수빈
+ * 내용 : 배열 초기화 후 출력
+ */
 public class p202 {
 
+	public static void main(String[] args) {
+		
+		int [] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		for(int i =0; i<10; i++ ) {
+			System.out.println(num[i]);
+	}
+}
 }
