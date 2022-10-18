c4ce9e21386c7f92bf727e7c2fc240dc17a4fea1
SleepingAlbaricoque <tnqls0421@naver.com>
2022-10-18 13:51:03 +0900

    2022.10.18 type casting exercise

---
+++ Helsinki_Mooc/src/part9/AnimalTest.java
@@ -0,0 +1,41 @@
+package part9;
+
+import java.util.ArrayList;
+
+class Animal {
+	public void move () {}
+}
+
+class Human extends Animal {
+	public void move () {
+		System.out.println("walk");
+	}
+	
+	public void read() {}
+}
+
+class Tiger extends Animal {
+	public void move () {
+		System.out.println("crawl");
+	}
+}
+
+class Eagle extends Animal {
+	public void move () {
+		System.out.println("fly");
+	}
+}
+
+public class AnimalTest {
+	public static void main(String[] args) {
+		ArrayList<Animal> aniList = new ArrayList<>(); // The type of the arraylist is Animal
+		aniList.add(new Human());
+		aniList.add(new Tiger());
+		aniList.add(new Eagle());
+		// aniList.get(0).read();
+
+		for(Animal ani : aniList) { // Because every element is stored as Animal type, this for loop works
+			ani.move();               // Each instance decides which overriding method to call
+		}
+	}
+}
