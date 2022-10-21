b2abc3fb416ca4012e2d7f4e71d1cf720c786e9e
SleepingAlbaricoque <tnqls0421@naver.com>
2022-10-21 13:54:54 +0900

    2022.10.21 interface exercises

---
+++ Helsinki_Mooc/src/part9/inheritance/taco_boxes/TacoBox.java
@@ -0,0 +1,6 @@
+package part9.inheritance.taco_boxes;
+
+public interface TacoBox {
+	int tacosRemaining();
+	void eat();
+}
---
+++ Helsinki_Mooc/src/part9/inheritance/taco_boxes/TripleTacoBox.java
@@ -0,0 +1,25 @@
+package part9.inheritance.taco_boxes;
+
+/* 날짜 : 2022.10.21
+ * 이름 : 조수빈
+ * 내용 : 인터페이스 구현 실습하기
+ */
+public class TripleTacoBox implements TacoBox{
+	private int taco;
+	
+	public TripleTacoBox() {
+		this.taco = 3;
+	}
+
+	@Override
+	public int tacosRemaining() {
+		return taco;
+	}
+
+	@Override
+	public void eat() {
+		if(taco >0)
+			taco -= 1;
+	}
+
+}
