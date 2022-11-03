318731eb05cf138ff9f1a2d83986da3f7323c414
SleepingAlbaricoque <tnqls0421@naver.com>
2022-11-04 01:18:30 +0900

    2022.11.04 regular expressions exercises

--- Helsinki_Mooc/src/part10/useful_techniques/regular_expressions/Checker.java
+++ Helsinki_Mooc/src/part10/useful_techniques/regular_expressions/Checker.java
@@ -1,6 +1,13 @@
 package part10.useful_techniques.regular_expressions;
 
 public class Checker {
+	private static Checker instance = new Checker();
+	private Checker() {}
+	
+	public static Checker getInstance() {
+		return instance;
+	}
+	
 	public boolean isDayofWeek(String string) {
 		String condition = "(mon|tue|wed|thu|fri|sat|sun)";
 		
@@ -11,9 +18,22 @@ public class Checker {
 	}
 	
 	public boolean allVowels(String string) {
-		String condition = "a*e*i*o*u*";
+		String condition = "[aeiou]+";
 		
 		if(string.toLowerCase().matches(condition))
+			return true;
+		
+		return false;
+	}
+	
+	public boolean timeOfDay(String string) {
+		String condition = "([01]\\d|2[0123]):([012345]\\d):([012345]\\d)";
+		/*  [01]: any number among 0 and 1
+		 *  \\d : any digit
+		 *  | : or
+		 *  2[0123] : any number among 0, 1, 2, 3 can follow the number 2 in the front   
+		 */
+		if(string.matches(condition))
 			return true;
 		
 		return false;
--- Helsinki_Mooc/src/part10/useful_techniques/regular_expressions/UI.java
+++ Helsinki_Mooc/src/part10/useful_techniques/regular_expressions/UI.java
@@ -8,46 +8,63 @@ public class UI {
 	
 	public UI() {
 		sc = new Scanner(System.in);
-		ch = new Checker();
+		ch = Checker.getInstance();
 	}
 	
-	public void start() {
-		System.out.println("Choose the function: ");
-		System.out.println("1: Day of Week");
-		System.out.println("2: Vowel Check");
-		System.out.println("3: Time of Day");
-		String opt = sc.nextLine();
-		switch(opt) {
-			case("1"):
-				while(true) {
-					System.out.print("Enter a string: ");
-					String answer = sc.nextLine();
-					if(ch.isDayofWeek(answer))
-						System.out.println("The form is correct.");
-					else
-						System.out.println("The form is incorrect.");
-					
-					if(answer.equals(""))
-						break;
+	public void start() {	
+		checker: while(true) {
+			System.out.println("Choose the function: ");
+			System.out.println("1: Day of Week");
+			System.out.println("2: Vowel Check");
+			System.out.println("3: Time of Day");
+			System.out.println("4: exit");
+			int opt = Integer.valueOf(sc.nextLine());
+			
+			switch(opt) {
+				case 1:
+					while(true) {
+						System.out.print("Enter a string: ");
+						String answer = sc.nextLine();
+						if(answer.equals(""))
+							break;
+						if(ch.isDayofWeek(answer))
+							System.out.println("The form is correct.");
+						else
+							System.out.println("The form is incorrect.");
+					}
 					break;
-				}
-			case("2"):
-				while(true) {
-					System.out.println("Enter a string: ");
-					String answer = sc.nextLine();
-					if(ch.allVowels(answer))
-						System.out.println("The form is correct.");
-					else
-						System.out.println("The form is incorrect.");
-					
-					if(answer.equals(""))
-						break;
+				case 2:
+					while(true) {
+						System.out.print("Enter a string: ");
+						String answer = sc.nextLine();
+						if(ch.allVowels(answer))
+							System.out.println("The form is correct.");
+						else
+							System.out.println("The form is incorrect.");
+						
+						if(answer.equals(""))
+							break;	
+					}
 					break;
-				}
-				
-		}
-		
-		
-		
+				case 3:
+					while(true) {
+						System.out.print("Enter a string: ");
+						String answer = sc.nextLine();
+						if(ch.timeOfDay(answer))
+							System.out.println("The form is correct.");
+						else
+							System.out.println("The form is incorrect.");
+						
+						if(answer.equals(""))
+							break;
+					}
+					break;
+				case 4: 
+					System.out.println("Powering off...");
+					break checker;
+				default:
+					System.out.println("Invalid Command");
+			}
+		}	
 	}
 }
